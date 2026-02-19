5c5afae85162a98112c90a377b814910ddd7094e
Andrei Khrapkov <144687861+tw1nflame@users.noreply.github.com>
2026-02-19 09:45:42 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,11 +1,9 @@
-# default_year в исходных данных: минимальный год, где dflt_year==1
-default_year = df.loc[df['dflt_year'] == 1].groupby('vat_num')['year'].min()
+tmp = df_final.reset_index()
 
-# есть ли строка за год до дефолта
-years_per_company = df.groupby('vat_num')['year'].apply(set)
-has_prev_row = default_year.index.to_series().apply(
-    lambda vat: (default_year[vat] - 1) in years_per_company[vat]
+duplicates = (
+    tmp[tmp['target'] == 1]
+    .groupby('vat_num')
+    .size()
 )
 
-print("Доля дефолтов, у которых есть строка за t-1:", has_prev_row.mean())
-print("Нет строки t-1 (кол-во):", (~has_prev_row).sum())
+print("Компаний с >1 строкой target=1:", (duplicates > 1).sum())
