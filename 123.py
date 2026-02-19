c95e426c6ac19f22be39964a12d0858a14198cb9
Andrei Khrapkov <144687861+tw1nflame@users.noreply.github.com>
2026-02-19 09:33:24 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,5 +1,11 @@
-import os
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
+# default_year в исходных данных: минимальный год, где dflt_year==1
+default_year = df.loc[df['dflt_year'] == 1].groupby('vat_num')['year'].min()
+
+# есть ли строка за год до дефолта
+years_per_company = df.groupby('vat_num')['year'].apply(set)
+has_prev_row = default_year.index.to_series().apply(
+    lambda vat: (default_year[vat] - 1) in years_per_company[vat]
+)
+
+print("Доля дефолтов, у которых есть строка за t-1:", has_prev_row.mean())
+print("Нет строки t-1 (кол-во):", (~has_prev_row).sum())
