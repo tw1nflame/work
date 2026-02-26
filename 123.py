459a3d516642e9797a614f64b3b147b931e96039
Andrei Khrapkov <144687861+tw1nflame@users.noreply.github.com>
2026-02-26 10:55:36 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,32 +1,31 @@
-top_features = [
-    "debt_to_total_liabilities",
-    "roa_ru_tax",
-    "absolute_liquidity",
-    "total_assets_turnover",
-    "receivables_to_total_assets",
-    "days_purch_outst",
-    "days_sales_outst",
-    "total_debt",
-    "payables_turnover",
-    "roe",
-    "2110_revenue",
-    "inventory_turnover",
-    "receivables_turnover",
-    "asset_conversion_period",
-    "cogs_to_revenue",
-    "roa",
-    "net_profit_margin",
-    "ebit",
-    "net_debt_to_equity",
-    "own_working_capital_adequacy",
-    "equity_concentration",
-    "days_inventory_outst",
-    "net_assets",
-    "working_capital",
-    "equity_maneuverability",
-    "fa_turnover_ratio",
-    "ebitm",
-    "debt_to_equity",
-    "gpm_on_sga",
-    "working_capital_to_total_assets",
-]
+-- подключиться к базе nwc (если выполняете из psql)
+\c nwc
+
+-- 1. создать роль (пользователя) без прав на создание объектов
+CREATE ROLE nwc_reader
+LOGIN
+PASSWORD 'STRONG_PASSWORD_HERE'
+NOSUPERUSER
+NOCREATEDB
+NOCREATEROLE
+NOINHERIT;
+
+-- 2. разрешить подключение к базе
+GRANT CONNECT ON DATABASE nwc TO nwc_reader;
+
+-- 3. доступ к схеме
+GRANT USAGE ON SCHEMA public TO nwc_reader;
+
+-- 4. доступ только на чтение ко всем существующим таблицам
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO nwc_reader;
+
+-- 5. доступ к последовательностям (если есть serial / identity)
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO nwc_reader;
+
+-- 6. права по умолчанию для будущих таблиц
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT ON TABLES TO nwc_reader;
+
+-- 7. права по умолчанию для будущих последовательностей
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT ON SEQUENCES TO nwc_reader;
