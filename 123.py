ecddcfc74208f5315d1803b00ba47ceefbcaa4a9
Andrei Khrapkov <144687861+tw1nflame@users.noreply.github.com>
2026-05-13 15:15:55 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,30 +1,13 @@
-from sklearn.linear_model import LinearRegression
+FROM node:20-alpine
 
+WORKDIR /app
 
-file = ''
-weights = []
-df = pd.read_excel(file)
-window_size = 9
-file_out = ''
+RUN npm config set strict-ssl false
+RUN npm install -g pnpm
 
-for _, row in list(df.iterrows())[window_size:]:
+COPY package.json pnpm-lock.yaml* ./
+RUN pnpm install
 
-    data = df.loc[df['Дата'] < row['Дата']]  # фильтруем данные до текущей даты
-    data = data.iloc[-window_size:]  # берем последние 9 месяцев
-    X = data[['y_ML', 'y_BaseModel']]
-    y = data['y_actual']
-    
-    model = LinearRegression()
-    model.fit(X, y)
+COPY . .
 
-    predict = model.predict(row[['y_ML', 'y_BaseModel']].to_frame().T)
-
-    df.loc[row.name, 'y_ensemble'] = predict[0]
-    weights.append((model.coef_[0], model.coef_[1], model.intercept_))
-
-weights_df = pd.DataFrame(weights, columns=['w_ML', 'w_BaseModel', 'intercept'])
-
-with pd.ExcelWriter(file_out, engine="openpyxl") as writer:
-    df.to_excel(writer, sheet_name="summary_with_ensemble", index=False)
-    weights_df.to_excel(writer, sheet_name="ensemble_weights", index=False)
-    
+RUN pnpm build
