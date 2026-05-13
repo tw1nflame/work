ab32e66994fb5f354a5731d7c11ef79379789cc8
Andrei Khrapkov <144687861+tw1nflame@users.noreply.github.com>
2026-05-13 15:22:44 +0300

    Update 123.py

--- 123.py
+++ 123.py
@@ -2,8 +2,11 @@ FROM node:20-alpine
 
 WORKDIR /app
 
+COPY .npmrc ./
+
 RUN npm config set strict-ssl false
-RUN npm install -g pnpm
+RUN npm config set registry https://nexus.npr.nornick.ru/repository/npm-public/
+RUN npm install -g pnpm@9.15.4
 
 COPY package.json pnpm-lock.yaml* ./
 RUN pnpm install
